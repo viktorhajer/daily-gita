702277a763276693f5ff79ca74052f5d52459f10
Viktor Hajer <viktor.hajer@evosoft.com>
2026-08-31 19:29:31 +0200

    new version++

---
+++ docs/chunk-BVSRiI3r.js
@@ -0,0 +1,1 @@
+import{T as jt,f as Rn,w as jg}from"./main-LBAWAYSG.js";var e=class t{static ɵfac=function(m){return new(m||t)};static ɵmod=Rn({type:t});static ɵinj=jt({imports:[jg,jg]})};export{e as t};
--- docs/chunk-CdJjdkOU.js
+++
@@ -1,1 +0,0 @@
-import{T as jt,f as Rn,w as jg}from"./main-SARHERDG.js";var e=class t{static ɵfac=function(m){return new(m||t)};static ɵmod=Rn({type:t});static ɵinj=jt({imports:[jg,jg]})};export{e as t};
